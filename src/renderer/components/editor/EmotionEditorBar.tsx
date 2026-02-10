f6da59002faec7f271e44e887d67971eac9b8ff4
Beak <chenyilong916002@gmail.com>
2026-02-10 21:53:38 +0800

    chore(emotion): remove console.log placeholder in EmotionEditorBar suggestions handler

--- src/renderer/components/editor/EmotionEditorBar.tsx
+++ src/renderer/components/editor/EmotionEditorBar.tsx
@@ -152,11 +152,7 @@ export const EmotionEditorBar: React.FC = () => {
                 `}
                 style={{ color: meta.color }}
                 onClick={() => {
-                  // 点击可以显示更多建议（未来可以扩展为详细建议面板）
-                  if (emotion.suggestions && emotion.suggestions.length > 0) {
-                    // 可以通过 toast 或其他方式显示建议
-                    console.log('Suggestions:', emotion.suggestions)
-                  }
+                  // TODO: 扩展为详细建议面板或 toast 通知
                 }}
               >
                 {extra.icon}
